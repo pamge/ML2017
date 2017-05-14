39365c492ad3e95457d62ff624a24a399b281456
pamge <cth.ufo@gmail.com>
2017-05-14 20:32:18 +0800

    [HW4] minor adjust

--- hw4/eoid.py
+++ hw4/eoid.py
@@ -4,7 +4,51 @@ import sys
 import math
 import numpy
 
+def elu(arr):
+	return numpy.where(arr > 0, arr, numpy.exp(arr) - 1)
+
+def make_layer(in_size, out_size):
+	w = numpy.random.normal(scale=0.5, size=(in_size, out_size))
+	b = numpy.random.normal(scale=0.5, size=out_size)
+	return (w, b)
+
+def forward(inpd, layers):
+	out = inpd
+	for layer in layers:
+		w, b = layer
+		out = elu(out @ w + b)
+	return out
+
+def gen_data(dim, layer_dims, N):
+	layers = []
+	data = numpy.random.normal(size=(N, dim))
+
+	nd = dim
+	for d in layer_dims:
+		layers.append(make_layer(nd, d))
+		nd = d
+	w, b = make_layer(nd, nd)
+	gen_data = forward(data, layers)
+	gen_data = gen_data @ w + b
+	return gen_data
+
+def train():
+	n = 20
+	avgs = []
+	layer_dims = [numpy.random.randint(60, 80), 100]
+	for dim in range(1, 61):
+		temp_avgs = []
+		for r in range(n):
+			data = gen_data(dim, layer_dims, numpy.random.randint(10000, 100000))
+			temp_avgs.append(numpy.average(numpy.std(data, axis = 0)))
+			print(str(dim) + ': ' + str(temp_avgs[-1]))
+		avgs.append(numpy.average(temp_avgs))
+	with open('model', 'w') as f:
+		for i in range(60):
+			f.write('%f\n' % round(avgs[i], 3))
+
 def main():
+	# train()
 	avgs = []
 	answers = []
 	data = numpy.load(sys.argv[1])
